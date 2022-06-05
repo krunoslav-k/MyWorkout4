2bf9d79f0e50bfcda1649939262aebbf6aac121f
Krunoslav Kazalicki <krunoslavk@hotmail.com>
2022-06-05 15:55:18 +0200

    firebase connceted and tested

--- app/src/main/java/hr/ferit/krunoslavkazalicki/myworkout4/MainActivity.kt
+++ app/src/main/java/hr/ferit/krunoslavkazalicki/myworkout4/MainActivity.kt
@@ -1,18 +1,22 @@
 package hr.ferit.krunoslavkazalicki.myworkout4
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val db = Firebase.firestore
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         val navController = findNavController(R.id.fragment)
 
@@ -20,5 +24,27 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         bottomNavigationView.setupWithNavController(navController)
+
+
+
+        //test
+        // Create a new user with a first and last name
+        val profile = hashMapOf(
+            "age" to 27,
+            "calorieIntake" to 2240,
+            "gender" to "male",
+            "height" to 189,
+            "weight" to 99
+        )
+
+// Add a new document with a generated ID
+        db.collection("profiles")
+            .add(profile)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
     }
 }
